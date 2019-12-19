eb0c17c68793fbe9007e4caf509cb222ba257c40
vijay ch <vijay.ch@kreatio.com>
2019-12-19 14:23:11 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,27 @@
+from flask import flash
+import re
+import subprocess
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+# App config.
+DEBUG = True
+app = Flask(__name__,template_folder='/home/vijaych/py-flask/templates')
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return render_template("index.html", resp="your script's output will be printed here")
+    if request.method == "POST":
+        resp = request.form["text_input"]
+        output = subprocess.check_output(['./script.sh',resp])
+    print(output)
+    return render_template("index.html", resp=output)
+
+@app.route('/reset', methods=['GET', 'POST'])
+def text():
+    if request.method == "POST":
+        return redirect("/")
+if __name__ == '__main__':
+   app.run(debug=True)
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "The argument you passed for your script was: $1"
