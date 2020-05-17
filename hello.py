492117188f1978016b86978087a9bd790149fd03
VijayChandamala <31991486+VijayChandamala@users.noreply.github.com>
2020-05-17 18:36:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 
 # App config.
 DEBUG = True
-app = Flask(__name__,template_folder='/app/py-flask/templates')
+app = Flask(__name__,template_folder='/app/templates')
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 @app.route('/', methods=['GET', 'POST'])
